d6c8b60df3ad05b8c73fc852d2266a994746bfc4
Linda Gibson <dakidz@live.com>
2014-07-21 00:32:55 -0700

    modifications on bdd spec and code to better complete jquery for assessment.

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,53 @@ var triangle = function(sideA, sideB, sideC) {
   var side1 = sideA;
   var side2 = sideB;
   var side3 = sideC;
-
-  var resultNoTriangle = false;
-  var resultEquilateral = false;
-  var resultIsosceles = false;
-  var resultScalene = false;
+  var angleType = ["noTriangle", "equilateral", "isosceles", "scalene"]
+  // var inputtedTriangle = [side1, side2, side3]
+  // var resultNoTriangle = false;
+  // var resultEquilateral = false;
+  // var resultIsosceles = false;
+  // var resultScalene = false;
 
   if((side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2) {
-      resultNoTriangle = true;
-      return resultNoTriangle;
+      angleType = "noTriangle";
+      return angleType;
   } else if (side1 === side2 && side2 === side3) {
-      resultEquilateral = true;
-      return resultEquilateral;
+      angleType = "equilateral";
+      return angleType;
   } else if (side1 === side2 || side2 === side3 || side1 === side3) {
-      resultIsosceles = true;
-      return resultIsosceles;
+      angleType = "isosceles";
+      return angleType;
   } else if (side1 !== side2 && side2 !== side3 && side3 !== side1) {
-      resultScalene = true;
-      return resultScalene;
+      angleType = "scalene";
+      return angleType;
   }
 
 };
+
+$(document).ready(function() {
+  $("form#triangleSideInput").submit(function(event) {
+    event.preventDefault();
+
+    var side1 = parseInt($("input#sideA").val());
+    var side2 = parseInt($("input#sideB").val());
+    var side3 = parseInt($("input#sideC").val()); 
+console.log("sides are okay");
+    var results = triangle(sideA, sideB, sideC);
+console.log("function ran");
+    var noTriangle = "You didn't enter a triangle.  Try again.";
+    var equilateral = "You have an Equilateral triangle!";
+    var isosceles = "You have an Isosceles triangle!";
+    var scalene = "You have a Scalene triangle!";
+ console.log("new variables okay");   
+
+    if (resultNoTriangle === true) {
+      $(".triangleType").text(noTriangle);
+      console.log("I found my answer");
+    }
+    $("#triangleResult").show();
+console.log("results okay");
+    $("input#sideA").val("");
+    $("input#sideB").val("");
+    $("input#sideC").val("");
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe("triangle", function() {
 	it("is not a triangle if the sum of two of its sides is less than or equal to the third side", function() {
-		triangle(2, 1, 5).should.equal(true);
+		triangle(2, 1, 5).should.equal("noTriangle");
 	});
 
 	it("is an equilateral triangle if all three sides are equal to each other", function () {
-		triangle(3, 3, 3).should.equal(true);
+		triangle(3, 3, 3).should.equal("equilateral");
 	});
 
 	it("is an isosceles triangle if two side are equal but their sum is greater than or equal to the third side", function() {
-		triangle(4, 4, 6).should.equal(true);
+		triangle(4, 4, 6).should.equal("isosceles");
 	});
 
 	it("is a scalene triangle if none of the sides are equal to each other", function() {
-		triangle(2, 5, 4).should.equal(true);
+		triangle(2, 5, 4).should.equal("scalene");
 	});
 });
