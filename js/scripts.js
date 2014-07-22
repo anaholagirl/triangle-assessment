501e57047b58c467b2f47327c681418d942ffa1a
Linda Gibson <dakidz@live.com>
2014-07-21 21:48:11 -0700

    last modifications to complete the triangle-assessment. Will define a triangle and refresh page.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,8 @@ var triangle = function(sideA, sideB, sideC) {
   var side1 = sideA;
   var side2 = sideB;
   var side3 = sideC;
-  var angleType = ["noTriangle", "equilateral", "isosceles", "scalene"]
-  // var inputtedTriangle = [side1, side2, side3]
-  // var resultNoTriangle = false;
-  // var resultEquilateral = false;
-  // var resultIsosceles = false;
-  // var resultScalene = false;
+  var angleType = ["noTriangle", "equilateral", "isosceles", "scalene"];
+  
 
   if((side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2) {
       angleType = "noTriangle";
@@ -27,29 +23,35 @@ var triangle = function(sideA, sideB, sideC) {
 };
 
 $(document).ready(function() {
+  
   $("form#triangleSideInput").submit(function(event) {
     event.preventDefault();
 
     var side1 = parseInt($("input#sideA").val());
     var side2 = parseInt($("input#sideB").val());
     var side3 = parseInt($("input#sideC").val()); 
-console.log("sides are okay");
-    var results = triangle(sideA, sideB, sideC);
-console.log("function ran");
-    var noTriangle = "You didn't enter a triangle.  Try again.";
-    var equilateral = "You have an Equilateral triangle!";
-    var isosceles = "You have an Isosceles triangle!";
-    var scalene = "You have a Scalene triangle!";
- console.log("new variables okay");   
+    var finalTriangle = triangle(side1, side2, side3);
 
-    if (resultNoTriangle === true) {
-      $(".triangleType").text(noTriangle);
-      console.log("I found my answer");
+    var resultNoTriangle = "You didn't enter a triangle.  Try again.";
+    var resultEquilateral = "You have an Equilateral triangle!";
+    var resultIsosceles = "You have an Isosceles triangle!";
+    var resultScalene = "You have a Scalene triangle!";
+    
+    if (finalTriangle === "noTriangle") {
+      $(".triangleType").text(resultNoTriangle);
+    } else if (finalTriangle === "equilateral") {
+        $(".triangleType").text(resultEquilateral);
+    } else if (finalTriangle === "isosceles") {
+        $(".triangleType").text(resultIsosceles);
+    } else  {
+      $(".triangleType").text(resultScalene);
     }
+    
     $("#triangleResult").show();
-console.log("results okay");
-    $("input#sideA").val("");
-    $("input#sideB").val("");
-    $("input#sideC").val("");
+
+    $(".btn-warning").click(function() {
+      $("#triangleResult").hide();
+      document.location.reload();
+    });
   });
 });
